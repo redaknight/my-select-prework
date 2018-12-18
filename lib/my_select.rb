117fa0ec60afba7c42bac89f0750300fa9c5d9b6
AL FATAH KADER <alfatah.kader@gmail.com>
2018-12-18 04:26:05 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,35 @@
 def my_select(collection)
- # your code here!
-end
+ 
+  if collection.count == 0
+    
+  	return nil
+  	
+  else
+    
+ 		counter = 0
+ 		
+ 		inner_array = [ ]
+ 		
+ 		while counter < collection.count
+ 		
+ 			if collection[counter] % 2 == 0
+ 			  
+ 				yield collection[counter]
+ 				
+ 				inner_array << collection[counter]
+ 				
+ 				counter += 1
+ 		
+ 		else
+ 			
+ 				puts collection[counter]
+ 				
+ 				counter += 1
+ 			end
+ 		
+ 		end
+ 		
+ 		return inner_array
+  	
+  	end
+ end	
